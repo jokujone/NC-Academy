0054d547952eb9f480d043319f8c70b7146dd014
Kimi <kimi.ihalainen@gmail.com>
2025-09-16 15:00:49 +0300

    harjoitus15

---
+++ Dotnet/merkkijonot/Program.cs
@@ -0,0 +1,21 @@
+﻿//First call needed to wake up the computer
+Runloops(10000, 'a', false);
+
+Runloops(10_000, 'a', true);
+Runloops(500_000, 'a', true);
+Runloops(600_000, 'a', true);
+
+void Runloops(int amount, char symbol, bool verbose)
+{
+    string result = "";
+    DateTime start = DateTime.Now;
+    for (int i = 0; i < amount; i++)
+    {
+        result += symbol;
+    }
+    if (verbose)
+        Console.WriteLine($"Time taken for {amount} of {symbol}: {Math.Round((DateTime.Now - start).TotalMilliseconds * 10) / 10}ms");
+    Random rnd = new Random();
+    if (rnd.NextDouble() > 0.95d)
+        Console.WriteLine($"Here's your entire {result}");
+}
