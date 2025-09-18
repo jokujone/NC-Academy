2980908fff7ff8a0d614e4204e988eae082e2d61
Kimi <kimi.ihalainen@gmail.com>
2025-09-18 11:20:18 +0300

    Added asp apps

---
+++ Dotnet/asp_testi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace asp_testi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Dotnet/asplaskin/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace asplaskin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
