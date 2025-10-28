7da32fb1e32ccafaa07ed5170554724bc33c4ee1
Kimi <kimi.ihalainen@gmail.com>
2025-10-28 10:11:23 +0200

    Added testing project

---
+++ Dotnet/TestiTestaus/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace TestiTestaus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Laske(20, 5));
+            Console.WriteLine(LaskeParemmin(20, 5));
+            Console.ReadLine();
+        }
+        public static int Laske(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int LaskeParemmin(int a, int b)
+        {
+            int aa = a + b;
+            int bee = a + b;
+            aa -= b;
+            bee -= a;
+
+            int baa = aa + bee;
+
+            Random randomRandomizer = new();
+            Random random = new Random(a + randomRandomizer.Next());
+            int ran = random.Next(10,500);
+
+            for (int i = 0; i < ran; i += 3)
+            {
+                int dom = random.Next(100,500000);
+                if (dom == 999)
+                {
+                    string run = "run";
+                    try
+                    {
+                        bool bees = int.TryParse(run, out baa);
+                        if (bees)
+                        {
+                            throw new Exception("a");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        if (e is Exception)
+                            continue;
+                    }
+                }
+            }
+            return baa;
+        }
+    }
+}
+
---
+++ Dotnet/TestiTestaus/TestiTestausTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// VSTest.Console.exe on työkalu testien suorittamiseen
+
+namespace TestiTestaus
+{
+    [TestClass]
+    public class TestiTestausTests
+    {
+        [TestMethod]
+        public void TestaaLaskemista()
+        {
+            int a = 5;
+            int b = 10;
+            int expected = 15;
+
+            int actual = Program.Laske(a, b);
+            Assert.AreEqual(expected, actual, "Laskeminen ei toimi :(");
+        }
+
+        [TestMethod]
+        public void TestaaParempaaLaskemista()
+        {
+            int a = 5;
+            int b = 10;
+            int expected = 15;
+
+            int actual = Program.Laske(a, b);
+            Assert.AreEqual(expected, actual, "Parempi laskeminen toimii huonommin:(");
+        }
+    }
+}
